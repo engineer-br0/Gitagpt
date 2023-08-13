d71687a493dffb9dc2da82fc6a192b341bc8d35a
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2023-08-14 00:45:59 +0530

    check

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ app.listen(PORT, (req, res)=>{
     console.log(`Webhook is running on port ${PORT}`);
 });
 
-app.get('webhook', (req, res)=>{
+app.get('/webhook', (req, res)=>{
     console.log("aa gyi req")
     let mode = req.query["hub.mode"];
     let challenge = req.query["hub.challenge"];
     let token = req.query['hub.verify_token'];
 
     console.log(mode, challenge, token);
+})
+
+app.get("/", (req, res)=>{
+    res.send("hii webhook setting")
 })
