6fee2786ac15fd2fbe9a1774e029ca0ea3ec013b
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2023-08-14 00:51:16 +0530

    verify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = 4000;
+const mytoken = "INDIA"
 
 app.listen(PORT, (req, res)=>{
     console.log(`Webhook is running on port ${PORT}`);
@@ -14,6 +15,15 @@ app.get('/webhook', (req, res)=>{
     let token = req.query['hub.verify_token'];
 
     console.log(mode, challenge, token);
+
+    if(mode && token){
+        if(mode === "subscribe" && token === mytoken){
+            res.status(200).send(challenge);
+        }
+        else{
+            res.status(404);
+        }
+    }
 })
 
 app.get("/", (req, res)=>{
